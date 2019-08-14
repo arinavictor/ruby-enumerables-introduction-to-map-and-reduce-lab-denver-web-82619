18000cb6860dfb464575de153f454dc39bfa9f3b
Arina Victor <arinavictor@yahoo.com>
2019-08-14 02:28:15 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -45,7 +45,11 @@ def map_to_square(source_array)
 end 
 
 def reduce_to_total(source_array, starting_point)
+  i = 0 
+  total = starting_point
   
+  while i < source_array.length do 
+    
   
 
 
