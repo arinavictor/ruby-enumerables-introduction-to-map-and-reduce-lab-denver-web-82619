be19448a91b1afdaf364b2b71b22fa5d09030d77
Arina Victor <arinavictor@yahoo.com>
2019-08-14 02:22:03 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -21,3 +21,18 @@ def map_to_no_change(source_array)
   end 
   new_array
 end 
+
+def map_to_double(source_array)
+  i = 0 
+  new_array = []
+  
+  while i < source_array.length do
+    new_array.push(2*source_array[i])
+    i += 1 
+  end 
+  new_array
+end 
+
+
+
+
